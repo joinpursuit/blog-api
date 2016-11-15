7c31c96e85c5f3f06134d9a6a0b3dba896eebaf2
nmadd <nate@fratmusic.com>
2016-11-15 15:33:14 -0500

    finish answers

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
-- get all blog posts
-- get specific blog post (by id)
-- get all blog posts that have 'React' in the title
-- get all blog posts and order by date
-- create a new blog post
-- update a blog post
-- delete a blog post 
-- get all authors
-- get all authors sorted alphabetically  
-- get specific author (by id)
-- get all comments
-- get specific comment (by id)
+## API Endpoints
+Create an api with the following endpoints:
+
+- x `/api/posts` get all blog posts
+- x `/api/posts/:id` get specific blog post (by id)   
+  - [Express params docs - Read the 'Route Parameters section'](https://expressjs.com/en/guide/routing.html)
+  - [Mongoose 'findById' docs](http://mongoosejs.com/docs/api.html#model_Model.findById)
+- x `/api/posts/sort/by-date` get all blog posts and order by date
+  - [Mongoose Sort Docs](http://mongoosejs.com/docs/api.html#query_Query-sort)
+  - [How to Sort By Date in Mongoose](http://stackoverflow.com/questions/5825520/in-mongoose-how-do-i-sort-by-date-node-js)
+- x `api/posts/sort/a-z` get all blog posts sorted alphabetically by title
+- x `/api/authors` get all authors
+- x `/api/authors/sort/a-z` get all authors sorted alphabetically  
+- x `/api/authors/:id` get specific author (by id)
+- x `/api/posts/tags/react` get all blog posts that have a 'react' tag
+  - [Mongoose Finding in Array](http://stackoverflow.com/questions/18148166/find-document-with-array-that-contains-a-specific-value)
+- x `/api/posts-with-authors` get all posts with author fully populated (in other words, the full author information should be displayed, including author name and id)
+  - [Mongoose 'population' docs](http://mongoosejs.com/docs/populate.html)
+- `/api/posts` POST a new blog post
+- `/api/posts/:id` DELETE a blog post
+  - [Mongoose remove Docs](http://mongoosejs.com/docs/api.html#query_Query-remove)
+- `/api/posts/:id` PUT (update) a blog post
+  - [Mongoose findOneAndUpdate docs](http://mongoosejs.com/docs/api.html#query_Query-findOneAndUpdate)
+
+## Bonuses
+- refactor your routes to use express router
+  - [Express Router Docs](http://expressjs.com/en/api.html#router)
+  - [Learn to Use Express Router](https://scotch.io/tutorials/learn-to-use-the-new-router-in-expressjs-4)
+- Create a 'tag search' field. add an index.html file with a text input box. When you type something into the input box and hit 'Submit', your API should search all your posts for any posts that have a tag that matches the input text
--- seed.js
+++ seed.js
@@ -6,18 +6,18 @@ const Post = models.Post;
 const Author = models.Author;
 
 const posts = [
-  {title: 'React is Awesome', body: 'React is the best. Heres why...'},
-  {title: 'jQuery 4ever', body: 'You think React is great? Heres why jQuery is all you need...'},
-  {title: 'The DOM Rocks', body: 'Heres why the DOM is better than React and jQuery combined'},
-  {title: 'Angular > Everything', body: 'Angular is the best framework for me...'},
-  {title: 'React is the Future', body: 'Ten reasons why React is the future of JavaScript frameworks'},
-  {title: 'Express from the Beginning', body: 'Heres how to setup Express from scratch'},
-  {title: 'SQL or NoSQL?', body: 'What type of database should you use?'},
-  {title: 'React, Angular, or Vue?', body: 'Comparison of top JavaScript frontend frameworks'},
-  {title: 'Building an Express, React, Mongo, Node App', body: 'Heres a guide on getting your fullstack JavaScript app up and running'},
-  {title: 'Realtime web with Socket.io and React', body: 'Info on how to get started with Socket.io and React'},
-  {title: 'JavaScript on the backend with Node js', body: 'Node is a whole new world for JavaScript'},
-  {title: 'Intro to JavaScript', body: 'Getting started with JavaScript'},
+  {title: 'React is Awesome', body: 'React is the best. Heres why...', tags: ['react', 'javascript', 'frontend']},
+  {title: 'jQuery 4ever', body: 'You think React is great? Heres why jQuery is all you need...', tags: ['react', 'javascript', 'jquery', 'frontend']},
+  {title: 'The DOM Rocks', body: 'Heres why the DOM is better than React and jQuery combined', tags: ['DOM', 'javascript', 'frontend']},
+  {title: 'Angular > Everything', body: 'Angular is the best framework for me...', tags: ['angular', 'javascript', 'frontend']},
+  {title: 'React is the Future', body: 'Ten reasons why React is the future of JavaScript frameworks', tags: ['react', 'javascript', 'frontend']},
+  {title: 'Express from the Beginning', body: 'Heres how to setup Express from scratch', tags: ['express', 'javascript', 'backend']},
+  {title: 'SQL or NoSQL?', body: 'What type of database should you use?', tags: ['databases', 'backend']},
+  {title: 'React, Angular, or Vue?', body: 'Comparison of top JavaScript frontend frameworks', tags: ['frameworks', 'javascript', 'frontend']},
+  {title: 'Building an Express, React, Mongo, Node App', body: 'Heres a guide on getting your fullstack JavaScript app up and running', tags: ['javascript', 'fullstack']},
+  {title: 'Realtime web with Socket.io and React', body: 'Info on how to get started with Socket.io and React', tags: ['react', 'javascript', 'frontend', 'socket.io']},
+  {title: 'JavaScript on the backend with Node js', body: 'Node is a whole new world for JavaScript', tags: ['javascript', 'backend']},
+  {title: 'Intro to JavaScript', body: 'Getting started with JavaScript', tags: ['javascript']},
 ];
 
 const authors = [
