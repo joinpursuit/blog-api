6b6e0fc38f6842fcfe87c72e50a56fd4de3e659f
jondubya86 <jonathanwu86@gmail.com>
2016-11-17 17:13:58 -0500

    whole day of work

--- models/post-model.js
+++ models/post-model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const postSchema = mongoose.Schema({
   title: {type: String, required: true},
   body: {type: String, required: true},
-  date: {type: Date, default: Date.now},
+  date: {type: Date, default: Date.call()},
   author: {type: mongoose.Schema.Types.ObjectId, ref: 'Author'},
   tags: [String]
 });
